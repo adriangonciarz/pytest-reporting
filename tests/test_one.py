ded032017e913382777c044a6f10b4c25ac15870
Adrian Gonciarz <adrian.gonciarz@appdate.pl>
2019-02-03 20:03:57 +0100

    few improvements

--- tests/test_one.py
+++ tests/test_one.py
@@ -6,6 +6,7 @@ from tests.test_base import TestBase
 class TestOne(TestBase):
     def test_a(self):
         response = requests.get(self.base_path + '/posts')
+        # This assertion should pass
         self.assert_response_status_code(response, 200)
 
     def test_b(self):
@@ -15,4 +16,5 @@ class TestOne(TestBase):
                    "body": "Whatever text"
                }
         response = requests.post(self.base_path + '/posts', json=body)
+        # OFC it will return 200 OK response
         self.assert_response_status_code(response, 202)
--- tests/test_two.py
+++ tests/test_two.py
@@ -5,6 +5,7 @@ from tests.test_base import TestBase
 
 class TestTwo(TestBase):
     def test_two_a(self):
-        response = requests.get(self.base_path + '/animals')
-        self.assert_response_status_code(response, 202)
+        response = requests.get(self.base_path + '/nonexisting-resource')
+        # There is no such resource so it will fail
+        self.assert_response_status_code(response, 200)
 
